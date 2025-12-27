9ff3930877291716aa738c8d7c076f3e6b71b0b2
Bankal-2708 <bankalmavi1@gmail.com>
2025-12-27 18:59:46 +0530

    Header Complete

--- Food App/src/App.jsx
+++ Food App/src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import Navbar from './Components/NavBar/Navbar'
+import Header from './Components/Header/Header'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Components/Pages/Home'
+import Menu from './Components/Pages/Menu'
+import Contact from './Components/Pages/Contact'
+import Mobile from './Components/Pages/Mobile'
 
 function App() {
   return (
     <div className='m-10'>
       <Navbar/>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/menu' element={<Menu/>}/>
+        <Route path='/mobile' element={<Mobile/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
     </div>
   )
 }
--- Food App/src/Components/Header/Header.jsx
+++ Food App/src/Components/Header/Header.jsx
@@ -1,15 +1,28 @@
 import React from "react";
-import Navbarimg from '../../assets/Navbar.jpg'
+import Navbarimg from "../../assets/Navbar.jpg";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+  const navigate = useNavigate()
   return (
     <div>
-      <div className="mt-10 ">
+      <div className="mt-10 mx-10">
         <img
           src={Navbarimg}
           alt=""
-          className="h-170 w-full object-cover rounded-xl"
+          className="h-170 w-full object-cover rounded-xl relative"
         />
+      </div>
+      <div className=" text-white max-w-[50%] absolute bottom-20 left-50 flex flex-col gap-8 items-start animate-[fadeIn_5s_ease-out_forwards]">
+        <h1 className="font-bold text-7xl leading-tight">Order your <br/>favourite food here</h1>
+        <p className="text-2xl">
+          Craving your favorite food? We deliver it fast. Choose from top
+          restaurants around you. Enjoy smooth ordering and secure payments.
+          Fresh, hot meals—anytime you want.
+        </p>
+        <button onClick={()=>navigate('/menu')} className="inline-block text-2xl font-semibold bg-white text-gray-600 px-6 py-2 rounded-2xl hover:scale-110 transition-transform ">
+          View Menu
+        </button>
       </div>
     </div>
   );
